d9aad83b4b42b13d73458e044b660639e8013f41
Eliju <ekola.elina@gmail.com>
2020-11-27 18:45:47 +0200

    1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -8,25 +8,35 @@ const Title = () => {
 }
 
 const Button = ({handleClick, text}) => <button onClick={handleClick} > {text} </button>
+
+const StatisticsHeader = () => (<h2>statistics</h2>)
 const Statistics = ({good, neutral, bad}) => {
-  return (
-    <>
-      <h2>statistics</h2>
-      good {good}<br/>
-      neutral {neutral}<br/>
-      bad {bad}<br/>
-      all {good + neutral + bad}<br/>
-      average {(good*1 + neutral*0 + bad*-1) / (good + neutral + bad)}<br/>
-      positive {good / (good + neutral + bad)} %
-    </>
-  )
+    if ((good===0) &&(neutral === 0) && (bad === 0)) {
+      return (
+        <div>
+          <StatisticsHeader />
+          No feedback given
+        </div>
+        )
+    }
+
+    return (
+      <>
+        <StatisticsHeader />
+        good {good}<br/>
+        neutral {neutral}<br/>
+        bad {bad}<br/>
+        all {good + neutral + bad}<br/>
+        average {(good*1 + neutral*0 + bad*-1) / (good + neutral + bad)}<br/>
+        positive {100 * good / (good + neutral + bad)}  %
+       </>
+      )    
 }
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
   const increaseBad = () => setBad(bad + 1)
