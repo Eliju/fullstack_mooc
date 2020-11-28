18f835b0ba9032f480d527d0d67ed7542e863197
Eliju <ekola.elina@gmail.com>
2020-11-28 19:37:05 +0200

    2.5

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,44 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = (props) => {
-  return (
-    <h2>{props.course.name}</h2>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.p.name} {props.p.exercises}
-    </p>
-  )
-}
-
-const Content = ({course}) => {
-  return (
-    <div>
-       {course.parts.map(p => <Part key={p.id} p={p} />)}
-    </div>
-  )
- }
-
-// reduce-function is already in use
-const Total = ({course}) => {
-  return (
-    <b>total of {course.parts.reduce((sum, item) => sum += item.exercises, 0 )} exercises</b>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-   </>
-  )
-}
+import Course from './components/Course';
 
 const CurriculumHeader = () => {
   return (
