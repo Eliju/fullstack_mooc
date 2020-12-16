4f3b1a83625e9014e75ae790daf19b6012d1d9ee
Eliju <36505898+Eliju@users.noreply.github.com>
2020-12-16 13:30:47 +0200

    4.3

---
+++ osa4/blogilista/tests/list_helper.test.js
@@ -0,0 +1,12 @@
+const { TestScheduler } = require('jest')
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+
+  expect(result).toBe(1)
+}
+
+)
