5356dd16af1c37a4fa6c25dba8e7e15517783087
Eliju <36505898+Eliju@users.noreply.github.com>
2020-12-20 15:13:59 +0200

    4.21*

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -41,34 +41,63 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const result = await Blog.findByIdAndDelete({ '_id': request.params.id })
-  response.json(result)
+  const decToken = jwt.verify(request.token, process.env.SECRET_VALUE)
+  if (!decToken.id) {
+    return response.status(401).json({ error: 'authorization token missing or invalid' })
+  }
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).json({ error: 'blog not found - it might be already deleted' })
+  } else {
+    if (blog.users[0].toString() === decToken.id) {
+      //const result = await Blog.findByIdAndDelete({ '_id': request.params.id })
+      const result = await blog.delete()
+      response.json(result)
+    } else {
+      return response.status(401).json({ error: 'not authorised to delete this blog' })
+    }
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
+  const decToken = jwt.verify(request.token, process.env.SECRET_VALUE)
+  if (!decToken.id) {
+    return response.status(401).json({ error: 'authorization token missing or invalid' })
+  }
   const data = request.body
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).json({ error: 'blog not found - might be deleted' })
+  }
+
   let updateBlog = {}
   if (data.likes > 0) {
     updateBlog = {
-      author: data.author,
-      title : data.title,
-      url: data.url,
-      likes: data.likes
+      author: data.author || blog.author,
+      title : data.title || blog.title,
+      url: data.url || blog.url,
+      likes: data.likes || blog.url,
+      users: blog.users
     }
   } else {
     updateBlog = {
-      author: data.author,
-      title : data.title,
-      url: data.url
+      author: data.author || blog.author,
+      title : data.title || blog.title,
+      url: data.url || blog.url,
+      users: blog.users
     }
   }
 
-  const updated = await Blog.findByIdAndUpdate(request.params.id, updateBlog, { new: true, runValidators: true })
+  if (blog.users[0].toString() === decToken.id) {
+    const updated = await Blog.findByIdAndUpdate(request.params.id, updateBlog, { new: true, runValidators: true })
 
-  if (updated) {
-    response.json(updated.toJSON())
+    if (updated) {
+      response.json(updated.toJSON())
+    } else {
+      response.status(404).end()
+    }
   } else {
-    response.status(404).end()
+    response.status(401).json({ error: 'authorization token missing or invalid' })
   }
 })
 
